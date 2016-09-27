b99e95133558d73d54b0c4ceb934bb275a85626f
alex <you@example.com>
2016-09-27 15:25:34 +0300

    done few migrations

---
+++ database/migrations/2016_09_27_114128_create_masterfiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMasterfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masterfiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sir_name',20);
+            $table->string('first_name',20);
+            $table->string('middle_name');
+            $table->date('dob');
+            $table->string('gender');
+            $table->integer('id_no');
+            $table->integer('school_id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('masterfiles');
+    }
+}
